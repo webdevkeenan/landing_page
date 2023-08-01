7327ac985dd8e928023f7f509b82fdbfa5f9722c
Keenan <42125735+webdevkeenan@users.noreply.github.com>
2023-08-01 15:31:14 -0400

    create javascript user toggle interaction

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,10 @@
+
+// Select Hamburger bars and Nav Links
 const hamburger = document.querySelector(".hamburger");
 const headerLinks = document.querySelector(".header__links");
 
+// When Hamburger is clicked toggle navbar
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+    headerLinks.classList.toggle("active");
 })
