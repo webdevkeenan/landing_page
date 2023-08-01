5671af003db24613f0805f792e0d024c67b19db4
Keenan <42125735+webdevkeenan@users.noreply.github.com>
2023-08-01 17:21:10 -0400

    remove extra lines of javascript

--- javascript/script.js
+++ javascript/script.js
@@ -3,8 +3,10 @@
 const hamburger = document.querySelector(".hamburger");
 const headerLinks = document.querySelector(".header__links");
 
+
 // When Hamburger is clicked toggle navbar
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     headerLinks.classList.toggle("active");
 })
+
